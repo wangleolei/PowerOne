079c1df0f2ef53000c9cba8591ddd764792b39b9
wangleolei <wanglei_leo@163.com>
2016-05-20 22:07:43 +0800

    Signed-off-by: wangleolei <wanglei_leo@163.com>

--- Base/Admin/Controller/ArticleController.class.php
+++ Base/Admin/Controller/ArticleController.class.php
@@ -323,4 +323,32 @@ $mpnews = $weixin->upload_news($news);
         else $this -> error('你的操作有错误！');
     }
 
+
+    public function uploadify()
+ {
+    var_dump("tseting");
+    if (!empty($_FILES)) {
+        var_dump("expression");
+        import("@.ORG.UploadFile");
+        $upload = new \Org\UploadFile();
+//        $upload = new \Think\Upload();
+        $upload->maxSize = 2048000;
+        $upload->allowExts = array('jpg','jpeg','gif','png');
+        $upload->savePath = "./upload/image/";
+        $upload->thumb = true; //设置缩略图
+        $upload->imageClassPath = "@.ORG.Image";
+        $upload->thumbPrefix = "130_,75_,24_"; //生成多张缩略图
+        $upload->thumbMaxWidth = "130,75,24";
+        $upload->thumbMaxHeight = "130,75,24";
+        $upload->saveRule = uniqid; //上传规则
+        $upload->thumbRemoveOrigin = true; //删除原图
+        if(!$upload->upload()){
+            $this->error($upload->getErrorMsg());//获取失败信息
+        } else {
+            $info = $upload->getUploadFileInfo();//获取成功信息
+        }
+        echo $info[0]['savename'];    //返回文件名给JS作回调用
+    }
+ }
+
 }
